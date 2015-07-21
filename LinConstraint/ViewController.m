6bfee79cf2dd3cce779485a9774ff6c16db19b7e
林君毅 <522526435@qq.com>
2015-07-21 17:12:09 +0800

    first commit

--- LinConstraint/ViewController.m
+++ LinConstraint/ViewController.m
@@ -12,11 +12,23 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    UIImageView *imageView1;
+    UIImageView *imageView2;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    imageView1 = [[UIImageView alloc] init];
+    imageView1.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:imageView1];
+    [imageView1 setTranslatesAutoresizingMaskIntoConstraints:NO]; //删除系统自动生成的约束，因为它们可能与自定义的约束冲突，从而导致自定义约束失效。
+    NSLayoutConstraint  *constraint = [NSLayoutConstraint constraintWithItem:imageView1 attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeHeight multiplier:0 constant:100];
+    [self.view addConstraint:constraint];
+    constraint = [NSLayoutConstraint constraintWithItem:imageView1 attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0];
+    [self.view addConstraint:constraint];
 }
 
 - (void)didReceiveMemoryWarning {
